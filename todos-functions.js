bc44e8b1b7a3a0a1876dc01378206fbc43b95a57
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-25 18:56:35 +0300

    Turnary Operator

--- todos-functions.js
+++ todos-functions.js
@@ -1,12 +1,7 @@
 // Get existing localStorage
 const getSavedTodos = () => {
 	const todosJSON = localStorage.getItem('todos');
-
-	if (todosJSON !== null) {
-		return JSON.parse(todosJSON);
-	} else {
-		return [];
-	}
+	return todosJSON !== null ? JSON.parse(todosJSON) : [];
 };
 
 // Save todos to localStorage
